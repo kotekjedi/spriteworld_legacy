26597aeb6d3df8f9124eab8f997cbcd3d8f69b02
Jack Brady <jack.brady4227@gmail.com>
2022-09-28 02:54:18 +0200

    Update factor_distributions.py

--- spriteworld/factor_distributions.py
+++ spriteworld/factor_distributions.py
@@ -140,11 +140,11 @@ class Beta(AbstractDistribution):
 
     if self.non_ident == False:
       if self.key == 'x' and n == 0:
-        self.alpha = 4
-        self.beta = 15
+        self.alpha = 4.
+        self.beta = 15.
       elif self.key == 'x' and n == 1:
-        self.alpha = 15
-        self.beta = 4
+        self.alpha = 15.
+        self.beta = 4.
       out = beta_dist(torch.tensor([self.alpha]), torch.tensor([self.beta])).sample()
       if self.key == 'x':
         out = out*0.55 + .2
